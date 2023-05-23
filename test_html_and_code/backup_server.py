d6a5fb1263c004e83149bbeed496ef717fae6f89
PromotezCitizen <gkstkdgus821@gmail.com>
2023-05-23 14:47:26 +0900

    [t] 로그 백업 테스트 성공

--- test_html_and_code/backup_server.py
+++ test_html_and_code/backup_server.py
@@ -5,9 +5,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-backup = Backup()
-
-backup.restore_dictionary()
+backup = Backup() # 이 경우 restore이 자동으로 발생
 backup_dict = backup.notification_logs
 
 print(backup_dict)
@@ -15,8 +13,45 @@ backup.print_dictionary()
 
 print(backup_dict is backup.notification_logs)
 
-backup_dict["qwer"] = 123456
+backup_dict["index"] = 3
+backup_dict["opo_uid1"] = {}
+backup_dict["opo_uid1"]["comment"] = {}
+backup_dict["opo_uid1"]["comment"]["1"] = "qwer"
+backup_dict["opo_uid1"]["comment"]["2"] = "asdf"
+backup_dict["opo_uid1"]["chat"] = {}
+backup_dict["opo_uid1"]["chat"]["my_uid_1"] = "chatting_sended_str_1"
+backup_dict["opo_uid1"]["chat"]["my_uid_2"] = "chatting_sended_str_2"
 
+"""
+logs - {
+        opo_uid_1: {
+            "comment": [
+                "comment_append_str_1", 
+                "comment_append_str_2", 
+                ...
+            ],
+            "chat": {
+                my_uid_1: "chatting_sended_str_1",
+                my_uid_2: "chatting_sended_str_2",
+                ...
+            }
+        },
+        opo_uid_2: {
+            "comment": [
+                "comment_append_str_1", 
+                "comment_append_str_2", 
+                ...
+            ],
+            "chat": {
+                my_uid_1: "chatting_sended_str_1",
+                my_uid_2: "chatting_sended_str_2",
+                ...
+            }
+        }
+    }
+"""
+
+print(backup.index)
 print(backup_dict)
 backup.print_dictionary()
 
