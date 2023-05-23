d8916452c8d53fc0107f6f6ca62b4098c7b06015
PromotezCitizen <gkstkdgus821@gmail.com>
2023-05-23 14:13:36 +0900

    [m] restore한 후 참조 딕셔너리와 원본 딕셔너리의 주소가 다른 것을 해결

--- test_html_and_code/backup_server.py
+++ test_html_and_code/backup_server.py
@@ -1,22 +1,23 @@
-from fastapi import FastAPI
-import atexit
 import json
 from utils.backup import Backup
-
+import atexit
+from fastapi import FastAPI
 
 app = FastAPI()
 
 backup = Backup()
 
-backup.print_dictionary()
-
+backup.restore_dictionary()
 backup_dict = backup.notification_logs
-backup_dict['test'] = 1234
 
 print(backup_dict)
 backup.print_dictionary()
 
-backup.restore_dictionary()
+print(backup_dict is backup.notification_logs)
 
+backup_dict["qwer"] = 123456
+
+print(backup_dict)
+backup.print_dictionary()
 
 atexit.register(backup.backup_dictionary)
