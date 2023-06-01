1aa8324810016f9c24e2576c9c97b7c7ae6ad92e
PromotezCitizen <gkstkdgus821@gmail.com>
2023-06-01 15:12:21 +0900

    [u] 소멸자 생성

--- utils/sse.py
+++ utils/sse.py
@@ -5,6 +5,10 @@ class ConnectionManager:
     def __init__(self):
         self.active_connections: Dict[str, asyncio.Queue] = {}
 
+    def __del__(self):
+        for client in self.active_connections:
+            del client
+
     async def connect(self, client_id: str):
         if client_id not in self.active_connections:
             self.active_connections[client_id] = asyncio.Queue()
