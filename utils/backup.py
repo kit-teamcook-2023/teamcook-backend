a58ab606e103092d785c1057046407aa9a18974c
PromotezCitizen <gkstkdgus821@gmail.com>
2023-06-13 00:33:11 +0900

    [m] 백업 데이터 형식 변경

--- utils/backup.py
+++ utils/backup.py
@@ -1,8 +1,6 @@
 import json
 
 class Backup():
-    index = 0
-
     def __init__(self):
         self.notification_logs = {}
         self.restore_dictionary()
@@ -12,15 +10,11 @@ class Backup():
         try:
             with open(file_path, "r") as file:
                 self.notification_logs = json.load(file)
-            
-            self.index = self.notification_logs["index"]
         except:
             print("file not exists")
-            self.index = 0
 
     def backup_dictionary(self):
         file_path = "backup.json"
-        self.notification_logs["index"] = self.index
         with open(file_path, "w") as file:
             json.dump(self.notification_logs, file)
 
